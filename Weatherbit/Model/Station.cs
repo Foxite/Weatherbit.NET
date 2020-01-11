c63b0e2a8a062c2c4dd4773c669e8a574b65b864
foxite <d.kok.2000@gmail.com>
2020-01-11 18:51:49 +0100

    Station class stub

---
+++ Weatherbit/Model/Station.cs
@@ -0,0 +1,13 @@
+﻿namespace Weatherbit.Model {
+	// weather station or airport
+	// TODO
+	public class Station {
+		public StationType Type { get; }
+		public string Code { get; }
+	}
+
+	public enum StationType {
+		WeatherStation,
+		Airport
+	}
+}
