c6ce5207d2fa0042a7021a3ab41fe0a5300cdd1b
foxite <d.kok.2000@gmail.com>
2020-01-11 18:51:40 +0100

    LicenseRequirementAttribute and License enum

---
+++ Weatherbit/License.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Weatherbit {
+	[Flags]
+	public enum License {
+		Free,
+		Starter,
+		Developer,
+		Advanced,
+		Enterprise
+	}
+}
---
+++ Weatherbit/LicenseRequirementAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Weatherbit {
+	[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+	public sealed class LicenseRequirementAttribute : Attribute {
+		public License LicenseRequired { get; }
+
+		public LicenseRequirementAttribute(License licenseRequired) {
+			LicenseRequired = licenseRequired;
+		}
+	}
+}
