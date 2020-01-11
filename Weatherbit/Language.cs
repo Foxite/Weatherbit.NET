5e022186ff2e907cbfa5e78259bb308ae4b46c7b
foxite <d.kok.2000@gmail.com>
2020-01-11 18:27:07 +0100

    Language and Units enums

---
+++ Weatherbit/Language.cs
@@ -0,0 +1,179 @@
+﻿namespace Weatherbit {
+	public enum Language {
+		/// <summary>
+		/// English
+		/// This is the default.
+		/// </summary>
+		EN,
+
+		/// <summary>
+		/// Arabic
+		/// </summary>
+		AR,
+
+		/// <summary>
+		/// Azerbaijani
+		/// </summary>
+		AZ,
+
+		/// <summary>
+		/// Belarusian
+		/// </summary>
+		BE,
+
+		/// <summary>
+		/// Bulgarian
+		/// </summary>
+		BG,
+
+		/// <summary>
+		/// Bosnian
+		/// </summary>
+		BS,
+
+		/// <summary>
+		/// Catalan
+		/// </summary>
+		CA,
+
+		/// <summary>
+		/// Czech
+		/// </summary>
+		CZ,
+
+		/// <summary>
+		/// Danish
+		/// </summary>
+		DA,
+
+		/// <summary>
+		/// German
+		/// </summary>
+		DE,
+
+		/// <summary>
+		/// Finnish
+		/// </summary>
+		FI,
+
+		/// <summary>
+		/// French
+		/// </summary>
+		FR,
+
+		/// <summary>
+		/// Greek
+		/// </summary>
+		EL,
+
+		/// <summary>
+		/// Estonian
+		/// </summary>
+		ET,
+
+		/// <summary>
+		/// Croation
+		/// </summary>
+		HR,
+
+		/// <summary>
+		/// Hungarian
+		/// </summary>
+		HU,
+
+		/// <summary>
+		/// Indonesian
+		/// </summary>
+		ID,
+
+		/// <summary>
+		/// Italian
+		/// </summary>
+		IT,
+
+		/// <summary>
+		/// Icelandic
+		/// </summary>
+		IS,
+
+		/// <summary>
+		/// Cornish
+		/// </summary>
+		KW,
+
+		/// <summary>
+		/// Lithuanian
+		/// </summary>
+		LT,
+
+		/// <summary>
+		/// Norwegian Bokmål
+		/// </summary>
+		NB,
+
+		/// <summary>
+		/// Dutch
+		/// </summary>
+		NL,
+
+		/// <summary>
+		/// Polish
+		/// </summary>
+		PL,
+
+		/// <summary>
+		/// Portuguese
+		/// </summary>
+		PT,
+
+		/// <summary>
+		/// Romanian
+		/// </summary>
+		RO,
+
+		/// <summary>
+		/// Russian
+		/// </summary>
+		RU,
+
+		/// <summary>
+		/// Slovak
+		/// </summary>
+		SK,
+
+		/// <summary>
+		/// Slovenian
+		/// </summary>
+		SL,
+
+		/// <summary>
+		/// Serbian
+		/// </summary>
+		SR,
+
+		/// <summary>
+		/// Swedish
+		/// </summary>
+		SV,
+
+		/// <summary>
+		/// Turkish
+		/// </summary>
+		TR,
+
+		/// <summary>
+		/// Ukrainian
+		/// </summary>
+		UK,
+
+		/// <summary>
+		/// Chinese (Simplified)
+		/// </summary>
+		ZH,
+
+		/// <summary>
+		/// Chinese (Traditional)
+		/// </summary>
+		ZH_TW
+	}
+}
---
+++ Weatherbit/Units.cs
@@ -0,0 +1,18 @@
+﻿namespace Weatherbit {
+	public enum Units {
+		/// <summary>
+		/// Celcius, meters, degrees
+		/// </summary>
+		Metric,
+
+		/// <summary>
+		/// Kelvin, meters, radians
+		/// </summary>
+		Scientific,
+
+		/// <summary>
+		/// Fahrenheit, miles, degrees
+		/// </summary>
+		Imperial
+	}
+}
