b295402668a70bac0c3828994c8f17d20446f942
foxite <d.kok.2000@gmail.com>
2020-01-11 18:26:55 +0100

    model City class with a few stub functions

---
+++ Weatherbit/Model/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Weatherbit.Model {
+	public class City {
+		public int ID { get; }
+		public string Name { get; }
+		public string CountryCode { get; }
+		public string? State { get; }
+
+		internal City(int id, string name, string countryCode, string? state) {
+			ID = id;
+			Name = name;
+			CountryCode = countryCode;
+			State = state;
+		}
+		
+		public static City GetCity(int id) => throw new NotImplementedException();
+		public static City[] GetCity(string name, string? state, string? countryCode) => throw new NotImplementedException();
+	}
+}
